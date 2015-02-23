5cd9973ceb7a56f37c1c8b83f776f34a91df3c89
Bret Comnes <bcomnes@gmail.com>
2015-02-23 12:29:50 -0800

    Newlines allowed after assignment operator

--- test/multiline.js
+++ test/multiline.js
@@ -1,37 +1,66 @@
 var test = require('tape')
 var fmt = require('../').transform
 
-test('multiline ', function (t) {
-  t.plan(3)
-
-  var program =
+var collapse = [
+  {
+    program:
     'var x = 1\n' +
     '\n' +
     '\n' +
-    'var z = 2\n'
+    'var z = 2\n',
 
-  var expected =
+    expected:
     'var x = 1\n' +
     '\n' +
-    'var z = 2\n'
+    'var z = 2\n',
 
-  var msg = 'two empty lines should collapse to one'
-  t.equal(fmt(program), expected, msg)
-
-  program =
+    msg: 'two empty lines should collapse to one'
+  },
+  {
+    program:
     'var x = 1\n' +
     '\n' + '\n' + '\n' + '\n' + '\n' +
     '\n' + '\n' + '\n' + '\n' + '\n' +
-    'var z = 2\n'
+    'var z = 2\n',
 
-  msg = 'ten empty lines should collapse to one'
-  t.equal(fmt(program), expected, msg)
-
-  program =
+    expected:
     'var x = 1\n' +
     '\n' +
-    'var z = 2\n'
+    'var z = 2\n',
 
-  msg = 'single empty line should be unmodified'
-  t.equal(fmt(program), program, msg)
+    msg: 'ten empty lines should collapse to one'
+  }
+]
+
+test('multiline collapse', function (t) {
+  t.plan(collapse.length)
+  collapse.forEach(function (obj) {
+    t.equal(fmt(obj.program), obj.expected, obj.msg)
+  })
+})
+
+var noops = [
+  {
+    program: 'var x = 1\n' +
+    '\n' +
+    'var z = 2\n',
+
+    msg: 'single empty line should be unmodified'
+  },
+  {
+    program:
+    'var cool =\n' +
+    'a +\n' +
+    'b +\n' +
+    'c\n',
+
+    msg: 'allow newlines after assignment operator'
+  }
+]
+
+test('multiline noop', function (t) {
+  t.plan(noops.length)
+  noops.forEach(function (obj) {
+    t.equal(fmt(obj.program), obj.program, obj.msg)
+  })
 })
